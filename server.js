5c07330fc766170d48ea84e81653854495023773
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-18 14:52:59 +0200

    add ws get

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
+app.get('/ws/quizz', (req, res) => {
+  res.json({ toto: 'titi' });
+});
 
 const www = '../quizz/dist/quizz';
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
